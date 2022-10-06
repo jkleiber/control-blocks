cc4c517edf2231e52e9d3916ab34a0f3558fca37
Justin Kleiber <jkleiber8@gmail.com>
2022-10-05 21:03:36 -0400

    Updates the linux build to work based on the windows changes

--- util/setup.sh
+++ util/setup.sh
@@ -12,5 +12,13 @@ else
     sudo apt install -y clang-format libusb-1.0-0-dev
 
     # Install gtk dependency for NFD library compilation
-    sudo apt-get install -y libgtk-3-dev
+    sudo apt-get install -y libgtk-3-dev build-essential gnome-devel
+
+    # Install Boost
+    wget -O boost_1_80_0.tar.gz https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz
+    tar xzvf boost_1_80_0.tar.gz
+    cd boost_1_80_0/
+    sudo apt-get install -y g++ python-dev autotools-dev libicu-dev libbz2-dev # build-essential installed earlier
+    ./bootstrap.sh --prefix=/usr/local/
+    sudo ./b2 install
 fi
